83d3a3a408a27ee4e62662399774c795a75cdd58
Alexander Kalinin <alex@kalinin.uk>
2021-07-10 12:31:31 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  },
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  plugins: ['jest'],
+  extends: ['airbnb-typescript/base', 'plugin:prettier/recommended'],
+  rules: {
+    // Disabling as it conflicts with prettier
+    '@typescript-eslint/indent': 'off'
+  }
+};
