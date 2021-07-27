3e0b6320b9f903554f6602fe8d99d0bd9bdc03b3
Alexander Kalinin <alex@kalinin.uk>
2021-07-27 20:46:32 +0100

    Revert "add tests for chrome path profile"
    This reverts commit 3cdc80ec57893ff5df460800ea08efde2b9a529e.

--- src/chrome/util.unit.test.ts
+++ src/chrome/util.unit.test.ts
@@ -14,133 +14,75 @@ describe('getDomain', () => {
 });
 
 describe('getPath', () => {
-  describe('default profile', () => {
-    it('should get correct windows path', async () => {
-      const originalPlatform = Object.getOwnPropertyDescriptor(
-        process,
-        'platform',
-      );
-      Object.defineProperty(process, 'platform', {
-        value: 'win32',
-      });
-
-      expect(getPath('Default')).toEqual(
-        `${homedir()}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies`,
-      );
-
-      Object.defineProperty(process, 'platform', {
-        value: originalPlatform,
-      });
+  it('should get correct windows path', async () => {
+    const originalPlatform = Object.getOwnPropertyDescriptor(
+      process,
+      'platform',
+    );
+    Object.defineProperty(process, 'platform', {
+      value: 'win32',
     });
 
-    it('should get correct macos path', async () => {
-      const originalPlatform = Object.getOwnPropertyDescriptor(
-        process,
-        'platform',
-      );
-      Object.defineProperty(process, 'platform', {
-        value: 'darwin',
-      });
+    expect(getPath('Default')).toEqual(
+      `${homedir()}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies`,
+    );
 
-      expect(getPath('Default')).toEqual(
-        `${homedir()}/Library/Application Support/Google/Chrome/Default/Cookies`,
-      );
-
-      Object.defineProperty(process, 'platform', {
-        value: originalPlatform,
-      });
-    });
-
-    it('should get correct linux path', async () => {
-      const originalPlatform = Object.getOwnPropertyDescriptor(
-        process,
-        'platform',
-      );
-      Object.defineProperty(process, 'platform', {
-        value: 'linux',
-      });
-
-      expect(getPath('Default')).toEqual(
-        `${homedir()}/.config/google-chrome/Default/Cookies`,
-      );
-
-      Object.defineProperty(process, 'platform', {
-        value: originalPlatform,
-      });
-    });
-
-    it('should throw if invalid os', () => {
-      const originalPlatform = Object.getOwnPropertyDescriptor(
-        process,
-        'platform',
-      );
-      Object.defineProperty(process, 'platform', {
-        value: 'freebsd',
-      });
-
-      expect(() => getPath('Default')).toThrow(
-        'Platform freebsd is not supported',
-      );
-
-      Object.defineProperty(process, 'platform', {
-        value: originalPlatform,
-      });
+    Object.defineProperty(process, 'platform', {
+      value: originalPlatform,
     });
   });
 
-  describe('custom profile', () => {
-    it('should get correct windows path', async () => {
-      const originalPlatform = Object.getOwnPropertyDescriptor(
-        process,
-        'platform',
-      );
-      Object.defineProperty(process, 'platform', {
-        value: 'win32',
-      });
-
-      expect(getPath('Custom')).toEqual(
-        `${homedir()}\\AppData\\Local\\Google\\Chrome\\User Data\\Custom\\Cookies`,
-      );
-
-      Object.defineProperty(process, 'platform', {
-        value: originalPlatform,
-      });
+  it('should get correct macos path', async () => {
+    const originalPlatform = Object.getOwnPropertyDescriptor(
+      process,
+      'platform',
+    );
+    Object.defineProperty(process, 'platform', {
+      value: 'darwin',
     });
 
-    it('should get correct macos path', async () => {
-      const originalPlatform = Object.getOwnPropertyDescriptor(
-        process,
-        'platform',
-      );
-      Object.defineProperty(process, 'platform', {
-        value: 'darwin',
-      });
+    expect(getPath('Default')).toEqual(
+      `${homedir()}/Library/Application Support/Google/Chrome/Default/Cookies`,
+    );
 
-      expect(getPath('Custom')).toEqual(
-        `${homedir()}/Library/Application Support/Google/Chrome/Custom/Cookies`,
-      );
+    Object.defineProperty(process, 'platform', {
+      value: originalPlatform,
+    });
+  });
 
-      Object.defineProperty(process, 'platform', {
-        value: originalPlatform,
-      });
+  it('should get correct linux path', async () => {
+    const originalPlatform = Object.getOwnPropertyDescriptor(
+      process,
+      'platform',
+    );
+    Object.defineProperty(process, 'platform', {
+      value: 'linux',
     });
 
-    it('should get correct linux path', async () => {
-      const originalPlatform = Object.getOwnPropertyDescriptor(
-        process,
-        'platform',
-      );
-      Object.defineProperty(process, 'platform', {
-        value: 'linux',
-      });
+    expect(getPath('Default')).toEqual(
+      `${homedir()}/.config/google-chrome/Default/Cookies`,
+    );
 
-      expect(getPath('Custom')).toEqual(
-        `${homedir()}/.config/google-chrome/Custom/Cookies`,
-      );
+    Object.defineProperty(process, 'platform', {
+      value: originalPlatform,
+    });
+  });
 
-      Object.defineProperty(process, 'platform', {
-        value: originalPlatform,
-      });
+  it('should throw if invalid os', () => {
+    const originalPlatform = Object.getOwnPropertyDescriptor(
+      process,
+      'platform',
+    );
+    Object.defineProperty(process, 'platform', {
+      value: 'freebsd',
+    });
+
+    expect(() => getPath('Default')).toThrow(
+      'Platform freebsd is not supported',
+    );
+
+    Object.defineProperty(process, 'platform', {
+      value: originalPlatform,
     });
   });
 });
