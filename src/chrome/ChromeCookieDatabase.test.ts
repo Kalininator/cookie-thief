d8587d711c5d158269140d1ee34887f6e6795915
Alexander Kalinin <alex@kalinin.uk>
2021-09-25 17:50:56 +0100

    tests

---
+++ src/chrome/ChromeCookieDatabase.test.ts
@@ -0,0 +1,78 @@
+import * as sqlite from 'better-sqlite3';
+import { ChromeCookieDatabase } from './ChromeCookieDatabase';
+
+jest.mock('better-sqlite3');
+
+describe('ChromeCookieDatabase', () => {
+  it('get single cookie', () => {
+    const getFn = jest.fn().mockReturnValue({
+      host_key: '.domain.com',
+      path: '/',
+      name: 'someCookie',
+      encrypted_value: 'encrypted_foo',
+    });
+    const prepareFn = jest.fn().mockReturnValue({ get: getFn });
+    (sqlite as unknown as jest.Mock).mockReturnValue({
+      prepare: prepareFn,
+    });
+
+    const path = '/some/path/db.sqlite';
+
+    const db = new ChromeCookieDatabase(path);
+
+    const cookie = db.findCookie('someCookie', '.domain.com');
+
+    expect(cookie).toEqual({
+      encrypted_value: 'encrypted_foo',
+      host_key: '.domain.com',
+      name: 'someCookie',
+      path: '/',
+    });
+    expect(getFn).toHaveBeenCalled();
+    expect(prepareFn).toHaveBeenCalledWith(
+      `SELECT host_key, path, is_secure, expires_utc, name, value, encrypted_value, creation_utc, is_httponly, has_expires, is_persistent FROM cookies where host_key like '%.domain.com' and name like '%someCookie' ORDER BY LENGTH(path) DESC, creation_utc ASC`,
+    );
+    expect(sqlite as unknown as jest.Mock).toHaveBeenCalledWith(path, {
+      readonly: true,
+      fileMustExist: true,
+    });
+  });
+
+  it('get all cookies', () => {
+    const allFn = jest.fn().mockReturnValue([
+      {
+        host_key: '.domain.com',
+        path: '/',
+        name: 'someCookie',
+        encrypted_value: 'encrypted_foo',
+      },
+    ]);
+    const prepareFn = jest.fn().mockReturnValue({ all: allFn });
+    (sqlite as unknown as jest.Mock).mockReturnValue({
+      prepare: prepareFn,
+    });
+
+    const path = '/some/path/db.sqlite';
+
+    const db = new ChromeCookieDatabase(path);
+
+    const cookie = db.listCookies();
+
+    expect(cookie).toEqual([
+      {
+        encrypted_value: 'encrypted_foo',
+        host_key: '.domain.com',
+        name: 'someCookie',
+        path: '/',
+      },
+    ]);
+    expect(allFn).toHaveBeenCalled();
+    expect(prepareFn).toHaveBeenCalledWith(
+      `SELECT host_key, path, is_secure, expires_utc, name, value, encrypted_value, creation_utc, is_httponly, has_expires, is_persistent FROM cookies`,
+    );
+    expect(sqlite as unknown as jest.Mock).toHaveBeenCalledWith(path, {
+      readonly: true,
+      fileMustExist: true,
+    });
+  });
+});
--- src/firefox/FirefoxCookieDatabase.test.ts
+++ src/firefox/FirefoxCookieDatabase.test.ts
@@ -4,7 +4,7 @@ import { FirefoxCookieDatabase } from './FirefoxCookieDatabase';
 jest.mock('better-sqlite3');
 
 describe('FirefoxCookieDatabase', () => {
-  it('should try reading db correctly', () => {
+  it('get single cookie', () => {
     const getFn = jest.fn().mockReturnValue({ value: 'foo' });
     const prepareFn = jest.fn().mockReturnValue({ get: getFn });
     (sqlite as unknown as jest.Mock).mockReturnValue({
@@ -21,6 +21,30 @@ describe('FirefoxCookieDatabase', () => {
     expect(getFn).toHaveBeenCalled();
     expect(prepareFn).toHaveBeenCalledWith(
       "SELECT value from moz_cookies WHERE name like 'someCookie' AND host like '%.domain.com'",
+    );
+    expect(sqlite as unknown as jest.Mock).toHaveBeenCalledWith(path, {
+      readonly: true,
+      fileMustExist: true,
+    });
+  });
+
+  it('get all cookies', () => {
+    const allFn = jest.fn().mockReturnValue([{ value: 'foo' }]);
+    const prepareFn = jest.fn().mockReturnValue({ all: allFn });
+    (sqlite as unknown as jest.Mock).mockReturnValue({
+      prepare: prepareFn,
+    });
+
+    const path = '/some/path/db.sqlite';
+
+    const db = new FirefoxCookieDatabase(path);
+
+    const cookie = db.listCookies();
+
+    expect(cookie).toEqual([{ value: 'foo' }]);
+    expect(allFn).toHaveBeenCalled();
+    expect(prepareFn).toHaveBeenCalledWith(
+      'SELECT name, value, host, path from moz_cookies',
     );
     expect(sqlite as unknown as jest.Mock).toHaveBeenCalledWith(path, {
       readonly: true,
