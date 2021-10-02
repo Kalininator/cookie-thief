a84d1ff05388975dc6102752ccd4f86767a4be09
Alexander Kalinin <alex@kalinin.uk>
2021-10-02 20:22:15 +0100

    lint

--- src/chrome/ChromeMacosCookieProvider.ts
+++ src/chrome/ChromeMacosCookieProvider.ts
@@ -1,7 +1,7 @@
 import { CookieProvider } from '../CookieProvider';
 import { Cookie } from '../types';
 import { ChromeCookie, ChromeCookieRepository } from './ChromeCookieRepository';
-import {decrypt} from './decrypt';
+import { decrypt } from './decrypt';
 import { getMacDerivedKey } from './getDerivedKey';
 
 const KEYLENGTH = 16;
