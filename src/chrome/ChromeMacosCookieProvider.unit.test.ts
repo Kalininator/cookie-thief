213fb9ee2012ce82947e5694f30b0b564d242d1b
Alexander Kalinin <alex@kalinin.uk>
2021-11-02 21:18:13 +0000

    Update ChromeMacosCookieProvider.unit.test.ts

--- src/chrome/ChromeMacosCookieProvider.unit.test.ts
+++ src/chrome/ChromeMacosCookieProvider.unit.test.ts
@@ -45,4 +45,56 @@ describe('chrome macos cookie provider', () => {
 
     expect(mockFindCookie).toHaveBeenCalledWith('some_cookie', '.test.com');
   });
+
+  it('should return undefined if cookie not found', async () => {
+    const mockFindCookie = jest.fn().mockReturnValue(undefined);
+    const mockDb: ChromeCookieRepository = {
+      findCookie: mockFindCookie,
+      listCookies(): ChromeCookie[] {
+        throw new Error('Function not implemented.');
+      },
+    };
+
+    const provider = new ChromeMacosCookieProvider(mockDb);
+
+    const cookie = await provider.getCookie('.test.com', 'some_cookie');
+
+    expect(cookie).toEqual(undefined);
+
+    expect(mockFindCookie).toHaveBeenCalledWith('some_cookie', '.test.com');
+  });
+
+  it('should list and decrypt cookies', async () => {
+    const mockListCookies = jest.fn().mockImplementation((): ChromeCookie[] => {
+      return [
+        {
+          host_key: '.some.url',
+          path: '/',
+          name: 'someCookie',
+          encrypted_value: fakeBuffer,
+        },
+      ];
+    });
+    const mockDb: ChromeCookieRepository = {
+      findCookie() {
+        throw new Error('function not implemented');
+      },
+      listCookies: mockListCookies,
+    };
+
+    const provider = new ChromeMacosCookieProvider(mockDb);
+
+    const cookies = await provider.listCookies();
+
+    expect(cookies).toEqual([
+      {
+        value: 'foo',
+        host: '.some.url',
+        path: '/',
+        name: 'someCookie',
+      },
+    ]);
+
+    expect(mockListCookies).toHaveBeenCalledWith();
+  });
 });
